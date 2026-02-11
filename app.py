ef60db27db1e76875875e50b1458a5db29253ea2
YaroslavGmyrya <jjaked25@gmail.com>
2026-02-11 20:04:37 +0700

    Update CI

--- app.py
+++ app.py
@@ -6,24 +6,15 @@ import json
 from fastapi import Body, status
 from fastapi.responses import JSONResponse
 
-# CTRL+C handler
-def handle_stop(signum, frame):
-    print("Stop server")
-    sys.exit(0)
- 
-signal.signal(signal.SIGINT, handle_stop)
-
-print("Start server")
-
 app = Flask(__name__)
 
-class User:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        self.id = str(uuid.uuid4())
+# class User:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#         self.id = str(uuid.uuid4())
         
-user_db = []
+# user_db = []
 
 
 @app.route("/")
@@ -34,38 +25,38 @@ def test():
         </div>
         """
 
-@app.get("/")
-async def get():
-    return JSONResponse(json.dumps(user_db))
+# @app.get("/")
+# async def get():
+#     return JSONResponse(json.dumps(user_db))
 
-@app.post("/")
-def add_user(data  = Body()):
-    user = User(data["name"], data["age"])
-    user.append(user)
-    return user
+# @app.post("/")
+# def add_user(data  = Body()):
+#     user = User(data["name"], data["age"])
+#     user.append(user)
+#     return user
 
-@app.put("/")
-def edit_user(data  = Body()):
-    target_id = data["id"]
+# @app.put("/")
+# def edit_user(data  = Body()):
+#     target_id = data["id"]
     
-    for user in user_db:
-        if user.id == target_id:
-            user.name = data["name"]
-            user.age = data["age"]
-            return user
+#     for user in user_db:
+#         if user.id == target_id:
+#             user.name = data["name"]
+#             user.age = data["age"]
+#             return user
         
-    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={ "message": "Пользователь не найден" })
+#     return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={ "message": "Пользователь не найден" })
 
-@app.delete("/")
-def delete_user(data  = Body()):
-    target_id = data["id"]
+# @app.delete("/")
+# def delete_user(data  = Body()):
+#     target_id = data["id"]
     
-    for user in user_db:
-        if user.id == target_id:
-            user_db.remove(user)
-            return user
+#     for user in user_db:
+#         if user.id == target_id:
+#             user_db.remove(user)
+#             return user
         
-    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={ "message": "Пользователь не найден" })
+#     return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={ "message": "Пользователь не найден" })
 
 
 if __name__ == "__main__":
